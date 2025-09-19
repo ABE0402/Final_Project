8cd5f9afe4e5e4f78aa6ebe227397d9ef5680d80
ABE0402 <cnrrnquddms@gmail.com>
2025-09-19 10:22:45 +0900

    Delete src directory

--- src/main/java/com/example/yori/domain/Gender.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yori.domain
-
-public enum class Gender {
-    MALE, FEMALE, UNKNOWN
-}
--- src/main/java/com/example/yori/repository/CafeRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.yori.repository;
-
-import com.example.yori.entity.Cafe;
-import com.example.yori.dto.SearchRequestDto;
-import java.util.List;
-
-public interface CafeRepositoryCustom {
-    List<Cafe> search(SearchRequestDto condition);
-}
